74a6fe9b85e62e46c311896a3889c7b12cf98865
Paige Godfrey <paagodfrey@gmail.com>
2020-08-22 20:44:05 -0700

    card model in progress

--- backend/models/card.js
+++ backend/models/card.js
@@ -2,7 +2,81 @@ const db = require("../db");
 
 class Card {
 
-  
+  /** Find all flash cards (can filter on terms in question). */
 
+  static async findAll(data) {
+    let baseQuery = `SELECT id, username, category, question, answer FROM cards`;
+    let whereExpressions = [];
+    let queryValues = [];
+
+    // For each possible search term, add to whereExpressions and
+    // queryValues so we can generate the right SQL
+
+    if (data.category) {
+      queryValues.push(data.category);
+      whereExpressions.push(`category = $${queryValues.length}`);
+    }
+
+    if (data.search) {
+      queryValues.push(`%${data.search}%`);
+      whereExpressions.push(`question ILIKE $${queryValues.length}`);
+    }
+
+    if (whereExpressions.length > 0) {
+      baseQuery += " WHERE ";
+    }
+
+    // Finalize query and return results
+
+    let finalQuery = baseQuery + whereExpressions.join(" AND ") + " ORDER BY question";
+    const cardsRes = await db.query(finalQuery, queryValues);
+    return cardsRes.rows;
+  }
+
+  /** Given a card id, return data about card. */
+
+  static async findOne(id) {
+    const cardRes = await db.query(
+      `SELECT id, username, category, question, answer
+              FROM cards
+              WHERE id = $1`,
+      [id]);
+
+    const card = cardRes.rows[0];
+
+    if (!card) {
+      const error = new Error(`There exists no flash-card '${id}'`);
+      error.status = 404;   // 404 NOT FOUND
+      throw error;
+    }
+
+    return card;
+  }
+
+  /** Create a flash card (from data), update db, return new card data. */
+
+  static async create(data) {
+    const duplicateCheck = await db.query(
+      `SELECT question 
+          FROM cards 
+          WHERE question = $1 AND username = $2`,
+      [data.question, data.username]);
+
+    if (duplicateCheck.rows[0]) {
+      let duplicateError = new Error(
+        `There already exists a flash card with question '${data.question}`);
+      duplicateError.status = 409; // 409 Conflict
+      throw duplicateError;
+    }
+
+    const result = await db.query(
+      `INSERT INTO cards 
+            (username, category, question, answer)
+          VALUES ($1, $2, $3, $4) 
+          RETURNING id, username, category, question, answer`,
+      [data.username, data.category, data.question, data.answer]);
+
+    return result.rows[0];
+  }
 
 }
